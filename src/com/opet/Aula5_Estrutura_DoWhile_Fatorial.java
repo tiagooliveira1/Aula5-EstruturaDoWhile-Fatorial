ada40034cf755b9a02f6fdd6449a19a9e71e2ac7
= <=>
2017-03-23 21:45:05 -0300

    Codigo com erros ainda

--- src/com/opet/Aula5_Estrutura_DoWhile_Fatorial.java
+++ src/com/opet/Aula5_Estrutura_DoWhile_Fatorial.java
@@ -1,11 +1,26 @@
 package com.opet;
 
+import com.opet.util.Reader;
 public class Aula5_Estrutura_DoWhile_Fatorial
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        // TODO Auto-generated method stub
+        int numeroAnterior = 0;
+        int soma = 0;
+
+        System.out.print("Insira um número:");
+        int numeroUsuario = Reader.readInt();
+        int i=numeroUsuario;
+        do {
+                numeroAnterior = numeroUsuario-(numeroUsuario-i)-1;
+                soma=soma+(numeroUsuario*numeroAnterior);
+                System.out.println(numeroUsuario+" * "+
+                  numeroAnterior+": "+(numeroUsuario*numeroAnterior));
+                soma=soma+(numeroUsuario*numeroAnterior);
+            System.out.println("Fatorial de "+numeroUsuario+" é "+soma);
+            i--;
+        } while(i>0);
 
     }
 
